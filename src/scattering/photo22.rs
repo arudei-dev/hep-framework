6bb288bf3fc2ce533052aa465143b1b1319bc864
hello-wahyu <al.arudei01@gmail.com>
2021-04-28 22:23:03 +0700

    Separated ReactionData into its own submodule

--- src/scattering/photo22.rs
+++ src/scattering/photo22.rs
@@ -1,7 +1,9 @@
 use super::*;
 
 pub mod base;
+pub mod rx_data;
 pub mod cross_section;
 
 pub use base::*;
+pub use rx_data::*;
 pub use cross_section::*;
--- src/scattering/photo22/base.rs
+++ src/scattering/photo22/base.rs
@@ -1,28 +1,6 @@
-use crate::quantum::observable::*;
 use crate::quantum::props::*;
 use super::*;
 
-
-pub struct ReactionData {
-    pub rx_type: Reaction,
-    pub p_inc_a: Particle,
-    pub p_inc_b: Particle,
-    pub p_out_c: Particle,
-    pub p_out_d: Particle,
-}
-
-impl ReactionData {
-    #[inline]
-    pub fn init(rx_type: Reaction) -> Self {
-        Self {
-            rx_type,
-            p_inc_a: Particle::zero(),
-            p_inc_b: Particle::zero(),
-            p_out_c: Particle::zero(),
-            p_out_d: Particle::zero(),
-        }
-    }
-}
 
 pub trait PhotoproductionBase {
     fn set_energy(&mut self, energy: Energy);
