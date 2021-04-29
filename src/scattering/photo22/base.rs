58d281c9ed96171c4c811b6fe61589cb094d0d4a
hello-wahyu <al.arudei01@gmail.com>
2021-04-29 10:17:17 +0700

    Extracted add_exchange and clear_exchanges

--- src/scattering/photo22/base.rs
+++ src/scattering/photo22/base.rs
@@ -2,6 +2,10 @@ use super::*;
 
 
 pub trait PhotoproductionBase {
+    fn add_exchange(&mut self, xch: Box<dyn xch_base::XchBase>);
+
+    fn clear_exchanges(&mut self);
+
     fn get_reaction_data(&self) -> &ReactionData;
 
     fn calc_amplitude_avgsq(&self) -> Real;
