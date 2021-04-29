82bfa34d7b66e3fd466c625f66ba83559d1ebe10
hello-wahyu <al.arudei01@gmail.com>
2021-04-29 10:32:25 +0700

    Deprecated add_exchange and clear_exchanges (requires trait upcasting)

--- src/scattering/photo22/base.rs
+++ src/scattering/photo22/base.rs
@@ -2,10 +2,6 @@ use super::*;
 
 
 pub trait PhotoproductionBase {
-    fn add_exchange(&mut self, xch: Box<dyn xch_base::XchBase>);
-
-    fn clear_exchanges(&mut self);
-
     fn get_reaction_data(&self) -> &ReactionData;
 
     fn calc_amplitude_avgsq(&self) -> Real;
